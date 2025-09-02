c03645b93786bdba361d235babf1add25db7e1a0
Kamoru AbdulRasak Oyinlola <rkamoru1@gmail.com>
2025-09-02 10:10:46 +0100

    Made some changes in app.js file

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-console.log("Git ");
+const barakat_current_course = "Python";
+if(barakat_current_course == "Python"){
+    console.log("That's True")
+} else {
+    console.log("What course are you doing?")
+}
