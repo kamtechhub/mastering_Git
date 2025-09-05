4a42f444d2443f5066d13950e4cf8a8eb7052bab
Kamoru AbdulRasak Oyinlola <rkamoru1@gmail.com>
2025-09-05 08:58:59 +0100

    made some changes

--- app.js
+++ app.js
@@ -1,13 +1,3 @@
-const abdul_current_course = "Git";
-const barakat_current_course = "Python";
-if(abdul_current_course == "Git"){
-    console.log("That's True abdul")
-} else if(barakat_current_course = "Python"){
-     console.log("That's True Barakat")
-} else {
-    console.log("What course are you doing?")
-}
-
 const name = 'AbdulRasak';
 console.log(name);
 
